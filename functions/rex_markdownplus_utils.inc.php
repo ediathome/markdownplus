cc7883548352ac90c091d10bedaac89141d0f39e
Martin Kolb <admin@vt-learn.de>
2015-11-04 13:45:23 +0100

    Removed additional slashes.

--- functions/rex_markdownplus_utils.inc.php
+++ functions/rex_markdownplus_utils.inc.php
@@ -6,8 +6,8 @@ function rex_markdownplus_utils_add_header_files($params)
 
 	$include_path = $REX['HTDOCS_PATH'].'/files/addons/markdownplus/markitup/';
 
-    $params['subject'] .= "\n  ". '<link rel="stylesheet" type="text/css" href="'.$include_path.'/skins/markitup/style.css" />';
-    $params['subject'] .= "\n  ". '<link rel="stylesheet" type="text/css" href="'.$include_path.'/sets/markdown/style.css" />';
+    $params['subject'] .= "\n  ". '<link rel="stylesheet" type="text/css" href="'.$include_path.'skins/markitup/style.css" />';
+    $params['subject'] .= "\n  ". '<link rel="stylesheet" type="text/css" href="'.$include_path.'sets/markdown/style.css" />';
     $params['subject'] .= "\n  ". '<script type="text/javascript" src="'.$include_path.'jquery.markitup.js"></script>';
     $params['subject'] .= "\n  ". '<script type="text/javascript" src="'.$include_path.'sets/markdown/set.js"></script>';
 	$params['subject'] .= "\n  ". '<script language="javascript">
