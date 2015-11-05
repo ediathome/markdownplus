5a42ea72ef23d2eb2f7907a64b0baeace62cc0de
Martin Kolb <admin@vt-learn.de>
2015-11-05 09:05:09 +0100

    Changed markdown for h1 and h2. Fixes bug in Firefox.

--- files/markitup/sets/markdown/set.js
+++ files/markitup/sets/markdown/set.js
@@ -14,8 +14,8 @@ myMarkdownSettings = {
 	previewParserPath:	'',
 	onShiftEnter:		{keepDefault:false, openWith:'\n\n'},
 	markupSet: [
-		{name:'First Level Heading', key:'1', placeHolder:'Your title here...', closeWith:function(markItUp) { return miu.markdownTitle(markItUp, '=') } },
-		{name:'Second Level Heading', key:'2', placeHolder:'Your title here...', closeWith:function(markItUp) { return miu.markdownTitle(markItUp, '-') } },
+		{name:'First Level Heading', key:'1', openWith: '# ', placeHolder:'Your title here...' },
+		{name:'Second Level Heading', key:'2', openWith: '## ', placeHolder:'Your title here...' },
 		{name:'Heading 3', key:'3', openWith:'### ', placeHolder:'Your title here...' },
 		{name:'Heading 4', key:'4', openWith:'#### ', placeHolder:'Your title here...' },
 		{name:'Heading 5', key:'5', openWith:'##### ', placeHolder:'Your title here...' },
