80b9f64f9c2fa948072d3ab034741e4945a0bf20
Martin Kolb <admin@vt-learn.de>
2015-11-07 17:48:20 +0100

    Made syntax pass jsLint test.

--- files/markitup/sets/markdown/set.js
+++ files/markitup/sets/markdown/set.js
@@ -1,3 +1,6 @@
+/*global
+jQuery, $, alert
+*/
 // -------------------------------------------------------------------
 // markItUp!
 // -------------------------------------------------------------------
@@ -12,46 +15,50 @@
 // -------------------------------------------------------------------
 var myMarkdownSettings = {
 	previewParserPath:	'',
-	onShiftEnter: {keepDefault:false, openWith:'\n\n'},
-	onTab: { 
-		keepDefault: false, 
-		replaceWith: '\t'
-	},
+	onShiftEnter: {keepDefault: false, openWith: '\n\n'},
+	onTab: {keepDefault: false, replaceWith: '\t'},
 	markupSet: [
-		{name:'First Level Heading', key:'1', openWith: '# ', placeHolder:'Your title here...' },
-		{name:'Second Level Heading', key:'2', openWith: '## ', placeHolder:'Your title here...' },
-		{name:'Heading 3', key:'3', openWith:'### ', placeHolder:'Your title here...' },
-		{name:'Heading 4', key:'4', openWith:'#### ', placeHolder:'Your title here...' },
-		{name:'Heading 5', key:'5', openWith:'##### ', placeHolder:'Your title here...' },
-		{name:'Heading 6', key:'6', openWith:'###### ', placeHolder:'Your title here...' },
-		{separator:'---------------' },		
-		{name:'Bold', key:'B', openWith:'**', closeWith:'**'},
-		{name:'Italic', key:'I', openWith:'_', closeWith:'_'},
-		{separator:'---------------' },
-		{name:'Bulleted List', openWith:'- ' },
-		{name:'Numeric List', openWith:function(markItUp) {
-			return markItUp.line+'. ';
+		{name: 'First Level Heading', key: '1', openWith: '# ', placeHolder: 'Your title here...' },
+		{name: 'Second Level Heading', key: '2', openWith: '## ', placeHolder: 'Your title here...' },
+		{name: 'Heading 3', key: '3', openWith: '### ', placeHolder: 'Your title here...' },
+		{name: 'Heading 4', key: '4', openWith: '#### ', placeHolder: 'Your title here...' },
+		{name: 'Heading 5', key: '5', openWith: '##### ', placeHolder: 'Your title here...' },
+		{name: 'Heading 6', key: '6', openWith: '###### ', placeHolder: 'Your title here...' },
+		{separator: '---------------' },
+		{name: 'Bold', key: 'B', openWith: '**', closeWith: '**'},
+		{name: 'Italic', key: 'I', openWith: '_', closeWith: '_'},
+		{separator: '---------------' },
+		{name: 'Bulleted List', openWith: '- ' },
+		{name: 'Numeric List', openWith: function (markItUp) {
+			"use strict";
+			return markItUp.line + '. ';
 		}},
-		{separator:'---------------' },
-		{name:'Picture', key:'P', replaceWith:'![[![Alternative text]!]]([![Url:!:http://]!])'},
-		{name:'Link', key:'L', openWith:'[', closeWith:']([![Url:!:http://]!])', placeHolder:'Your text to link here...' },
-		{separator:'---------------'},
-		// {name:'Quotes', openWith:'> '}, # disable quotes for now, as they do not work
-		{name:'Code Block / Code', openWith:'(!(\t|!|`)!)', closeWith:'(!(`)!)'},
-		{name:'Table', openWith:'|thead1 | thead2 | thead3 | thead4 |\n|-----|------|------|------|\n|cell1 |cell2 |cell3 |cell4 |\n|cell1 |cell2 |cell3 |cell4 |' },
-		{separator:'---------------'},
-		// {name:'Preview', call:'preview', className:"preview"}
+		{separator: '---------------' },
+		{name: 'Picture', key: 'P', replaceWith: '![[![Alternative text]!]]([![Url:!:http://]!])'},
+		{name: 'Link', key: 'L', openWith: '[', closeWith: ']([![Url:!:http://]!])', placeHolder: 'Your text to link here...' },
+		{name: 'RedaxoLink',
+			beforeInsert: function () {
+				"use strict";
+				alert("Insert redaxo link");
+			}},
+		{separator: '---------------'},
+		// {name: 'Quotes', openWith: '> '}, # disable quotes for now, as they do not work
+		{name: 'Code Block / Code', openWith: '(!(\t|!|`)!)', closeWith: '(!(`)!)'},
+		{name: 'Table', openWith: '|thead1 | thead2 | thead3 | thead4 |\n|-----|------|------|------|\n|cell1 |cell2 |cell3 |cell4 |\n|cell1 |cell2 |cell3 |cell4 |' },
+		{separator: '---------------'}
+		// {name: 'Preview', call: 'preview', className:"preview"}
 	]
-}
+};
 
 // mIu nameSpace to avoid conflict.
-miu = {
-	markdownTitle: function(markItUp, char) {
-		heading = '';
-		n = $.trim(markItUp.selection||markItUp.placeHolder).length;
-		for(i = 0; i < n; i++) {
+var miu = {
+	markdownTitle: function (markItUp, char) {
+		"use strict";
+		var heading = '', n, i;
+		n = jQuery.trim(markItUp.selection || markItUp.placeHolder).length;
+		for (i = 0; i < n; i + 1) {
 			heading += char;
 		}
-		return '\n'+heading;
+		return '\n' + heading;
 	}
-}
+};
